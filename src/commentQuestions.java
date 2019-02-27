46ab2ae7b31baed01a532279b25910d6cab4c2f1
Naxhi <47636876+Naxhi@users.noreply.github.com>
2019-02-27 13:40:52 -0500

    Split up parts of the big question generator into unit specific java files for simplicity's sake

---
+++ src/commentQuestions.java
@@ -0,0 +1,56 @@
+import java.util.Random;
+
+public class commentQuestions {
+    Random rand;
+
+    private String[] commentQuestions = {
+            "This is a comment",
+            "This is\nA comment",
+            "Author: Student\nClass: COMP 101\nDescription: ---",
+            "This code will do [x]",
+            "This code will do [x]\nIt will also do [y]"
+    };
+    private String[] generalCommentAnswers = {"Single-Line Comment", "Multi-Line Comment", "Java-Doc Comment", "Unknown"};
+
+    private String correctAnswer;
+    private String question = "";
+
+    public commentQuestions(){
+        rand = new Random();
+    }
+
+    public void generateGeneralCommentQuestion(){
+        int random;
+
+        do {
+            random = rand.nextInt(5);
+            question = commentQuestions[random];
+            commentQuestions[random] = "";
+        }while(question.equals(""));
+
+        question = question+"\n\nWhat would you use to comment the sentence above?";
+
+        switch(random){
+            case 0: correctAnswer = generalCommentAnswers[0];
+                break;
+            case 1: correctAnswer = generalCommentAnswers[1];
+                break;
+            case 2: correctAnswer = generalCommentAnswers[2];
+                break;
+            case 3: correctAnswer = generalCommentAnswers[0];
+                break;
+            case 4: correctAnswer = generalCommentAnswers[1];
+                break;
+        }
+    }
+
+    public String[] getCommentAnswers(){ return generalCommentAnswers; }
+
+    public String getQuestion(){
+        return question;
+    }
+
+    public String getCorrectAnswer(){
+        return correctAnswer;
+    }
+}
--- src/objectComparisonQuestions.java
+++ src/objectComparisonQuestions.java
@@ -1,18 +1,8 @@
 import java.util.Random;
-import java.util.ArrayList;
 
-public class QuestionGenerator {
+public class objectComparisonQuestions {
 
     Random rand;
-
-    private String[] commentQuestions = {
-            "This is a comment",
-            "This is\nA comment",
-            "Author: Student\nClass: COMP 101\nDescription: ---",
-            "This code will do [x]",
-            "This code will do [x]\nIt will also do [y]"
-    };
-    private String[] generalCommentAnswers = {"Single-Line Comment", "Multi-Line Comment", "Java-Doc Comment", "Unknown"};
 
     private String[] comparableStrings = {"Dog", "Cat", "Apple", "Pineapple", "Squid", "Bean",
             "Squirrel", "Chicken", "Zoo", "Dark", "Cute", "Ape", "Burger", "Pittsburgh", "Pennsylvania",
@@ -23,33 +13,8 @@ public class QuestionGenerator {
     private String correctAnswer;
     private String question = "";
 
-    public QuestionGenerator(){
+    public objectComparisonQuestions(){
         rand = new Random();
-    }
-
-    public void generateGeneralCommentQuestion(){
-        int random;
-
-        do {
-            random = rand.nextInt(5);
-            question = commentQuestions[random];
-            commentQuestions[random] = "";
-        }while(question.equals(""));
-
-        question = question+"\n\nWhat would you use to comment the sentence above?";
-
-        switch(random){
-            case 0: correctAnswer = generalCommentAnswers[0];
-                    break;
-            case 1: correctAnswer = generalCommentAnswers[1];
-                    break;
-            case 2: correctAnswer = generalCommentAnswers[2];
-                    break;
-            case 3: correctAnswer = generalCommentAnswers[0];
-                    break;
-            case 4: correctAnswer = generalCommentAnswers[1];
-                    break;
-        }
     }
 
     public void generateComparableQuestion(){
@@ -70,8 +35,6 @@ public class QuestionGenerator {
             correctAnswer = comparableStringAnswers[2];
         }
     }
-
-    public String[] getCommentAnswers(){ return generalCommentAnswers; }
 
     public String[] getComparableStringAnswers() { return comparableStringAnswers; }
 
