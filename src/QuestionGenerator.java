cb497714bc2a14afeb5aa98e4c1d6048fc78e4d5
Naxhi <47636876+Naxhi@users.noreply.github.com>
2019-02-27 11:37:13 -0500

    Added method to create questions to test user in understanding of "compareTo" function

--- src/QuestionGenerator.java
+++ src/QuestionGenerator.java
@@ -12,11 +12,15 @@ public class QuestionGenerator {
             "This code will do [x]",
             "This code will do [x]\nIt will also do [y]"
     };
-
     private String[] generalCommentAnswers = {"Single-Line Comment", "Multi-Line Comment", "Java-Doc Comment", "Unknown"};
 
-    private String correctAnswer;
+    private String[] comparableStrings = {"Dog", "Cat", "Apple", "Pineapple", "Squid", "Bean",
+            "Squirrel", "Chicken", "Zoo", "Dark", "Cute", "Ape", "Burger", "Pittsburgh", "Pennsylvania",
+            "Valentine", "Programming", "Computer"};
+    private String[] comparableStringAnswers = {"Positive", "Negative", "Zero", "Unknown"};
 
+    private char[] characters = {'+','-','*','/','%','<','=','>'};
+    private String correctAnswer;
     private String question = "";
 
     public QuestionGenerator(){
@@ -48,9 +52,28 @@ public class QuestionGenerator {
         }
     }
 
-    public String[] getCommentAnswers(){
-        return generalCommentAnswers;
+    public void generateComparableQuestion(){
+        int random = rand.nextInt(comparableStrings.length);
+        String firstPart = comparableStrings[random];
+        random = rand.nextInt(comparableStrings.length);
+        String secondPart = comparableStrings[random];
+
+        question = firstPart+".compareTo("+secondPart+");\nWhat would be the value of the number returned from this compareTo?";
+
+        if(firstPart.compareTo(secondPart)>0){
+            correctAnswer = comparableStringAnswers[0];
+        }
+        else if(firstPart.compareTo(secondPart)<0){
+            correctAnswer = comparableStringAnswers[1];
+        }
+        else{
+            correctAnswer = comparableStringAnswers[2];
+        }
     }
+
+    public String[] getCommentAnswers(){ return generalCommentAnswers; }
+
+    public String[] getComparableStringAnswers() { return comparableStringAnswers; }
 
     public String getQuestion(){
         return question;
