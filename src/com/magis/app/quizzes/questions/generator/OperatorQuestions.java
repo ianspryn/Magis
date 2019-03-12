2a6ccbd21fbd355e34b3217bbceeb53d18e14c2c
Naxhi <47636876+Naxhi@users.noreply.github.com>
2019-03-12 11:06:51 -0400

    Did a question generating engine for "Operator" questions

--- src/com/magis/app/quizzes/questions/generator/OperatorQuestions.java
+++ src/com/magis/app/quizzes/questions/generator/OperatorQuestions.java
@@ -1,6 +1,8 @@
 package com.magis.app.quizzes.questions.generator;
 
+import java.util.Collections;
 import java.util.Random;
+import java.util.ArrayList;
 
 public class OperatorQuestions {
     Random rand;
@@ -22,21 +24,117 @@ public class OperatorQuestions {
 
     public void getIntegerDivisionQuestion(){
         question = "";
-        int firstInt = 0, secondInt = 100;
-        double firstDouble = 0;
+        int firstInt, secondInt = 100;
+        double firstDouble;
 
-        question+="int value1 = "+secondInt+"\n";
+        ArrayList<String> shuffler = new ArrayList<>();
+
+        question+="int value1 = "+secondInt+";\n";
 
         int firstData = rand.nextInt(2);
         if(firstData>0){
             firstInt = rand.nextInt(100)+1;
-            question += "int value2 = " + firstInt;
+            question += "int value2 = " + firstInt+";\n";
+            correctAnswer = ""+(secondInt/firstInt);
+            shuffler.add(correctAnswer);
+            shuffler.add(""+(secondInt/(double)firstInt));
         }
         else{
             firstDouble = rand.nextDouble()*100;
-            question += "double value2 = " + firstDouble;
+            question += "double value2 = " + firstDouble+";\n";
+            correctAnswer = ""+(secondInt/firstDouble);
+            shuffler.add(correctAnswer);
+            shuffler.add(""+(secondInt/(int)firstDouble));
         }
 
+        for(int i=2; i<5; i++){
+            shuffler.add(""+(rand.nextInt(50)));
+        }
+        Collections.shuffle(shuffler);
+
+        answers = shuffler.toArray(answers);
+
+        question+="quotient = value1 / value2; \n\nWhat is the value of \"quotient\"?";
+    }
+
+    public void getIncrementalQuestion(){
+        question = "";
+        int number;
+        int extraNumber;
+        number = rand.nextInt(11);
+        ArrayList<String> shuffler = new ArrayList<>();
+
+        question += "int value = " + number;
+
+        int incrementSelector = rand.nextInt(incrementalEquations.length);
+
+        switch(incrementSelector){
+            case 0:
+                question += "\nvalue++;";
+                correctAnswer = ""+(number++);
+                shuffler.add(correctAnswer);
+                break;
+            case 1:
+                question += "\nvalue--;";
+                correctAnswer = ""+(number--);
+                shuffler.add(correctAnswer);
+                break;
+            case 2:
+                extraNumber = rand.nextInt(11);
+                question += "\nvalue += " + extraNumber+";";
+                number += extraNumber;
+                correctAnswer = ""+(number);
+                shuffler.add(correctAnswer);
+                break;
+            case 3:
+                extraNumber = rand.nextInt(11);
+                question += "\nvalue -= " + extraNumber+";";
+                number -= extraNumber;
+                correctAnswer = ""+(number);
+                shuffler.add(correctAnswer);
+                break;
+            case 4:
+                extraNumber = rand.nextInt(11);
+                question += "\nvalue *= " + extraNumber+";";
+                number *= extraNumber;
+                correctAnswer = ""+(number);
+                shuffler.add(correctAnswer);
+                break;
+            case 5:
+                extraNumber = rand.nextInt(11);
+                question += "\nvalue /= " + extraNumber+";";
+                number /= extraNumber;
+                correctAnswer = ""+(number);
+                shuffler.add(correctAnswer);
+                break;
+        }
+
+        shuffler.add(""+(number+=25));
+        shuffler.add(""+(number*=11));
+        shuffler.add(""+(number-=14));
+        Collections.shuffle(shuffler);
+        shuffler.add("Unknown");
+
+        answers = shuffler.toArray(answers);
+
+        question+="\n\nWhat is the value of \"value\" after this incremental equation?";
+    }
+
+    public void getModularQuestion(){
+        question = "";
+        int divisor = rand.nextInt(11);
+        int divider = rand.nextInt(11);
+        ArrayList<String> shuffler = new ArrayList<>();
+
+        question += ""+divisor+" % "+divider+"\n\nWhat is the result of this operation?";
+        correctAnswer = ""+(divisor%divider);
+        shuffler.add(correctAnswer);
+        shuffler.add((""+(divisor+divider)));
+        shuffler.add((""+(divisor*divider)));
+        shuffler.add((""+(divisor++)));
+        Collections.shuffle(shuffler);
+        shuffler.add("Unknown");
+        answers = shuffler.toArray(answers);
     }
 
     public String[] getAnswers() { return answers; }
