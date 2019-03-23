8795f4d17e59f4eb79e028cd5e71914e5fbe11ea
GCC\TANMR1 <tanmr1@gcc.edu>
2019-03-23 14:26:00 -0400

    Update studentResults.java

--- src/com/magis/app/results/studentResults.java
+++ src/com/magis/app/results/studentResults.java
@@ -16,14 +16,14 @@ public class StudentResults {
         this.UN = username;
     }
 
-    // return all exam results in a nice String
+
     public ArrayList<double[]> quizResults() {
 
         // each quizzesRes element has double[](avg, best, worst) scores
         ArrayList<double[]> quizzesRes = new ArrayList<double[]>();
 
         for (int i = 0; i < Main.lessonModel.getChapters().size(); i++) {
-            // default quiz vals
+            // default quiz result vals
             double [] quizRes = new double[3];
             Arrays.fill(quizRes, 0.0);
             String title = Main.lessonModel.getChapter(i).getTitle();
@@ -31,20 +31,37 @@ public class StudentResults {
                 StudentModel.Student.Quiz quiz = Main.studentModel.getStudent(Main.username).getQuiz(i);
 
                 quizRes[0] = quiz.getAverageScore();
-                ArrayList allScores = quiz.getScores();
-                quizRes[1] = Collections.max(allScores);
+                quizRes[1] = quiz.getBestScore();
+                quizRes[2] = quiz.getWorstScore();
             }
+
+            quizzesRes.add(quizRes);
         }
 
         return quizzesRes;
     }
 
-    // return all quiz results in a nice String
-    private  quizResults() {
-        String results = "";
-        // for each quiz
+    public ArrayList<double[]> testResults() {
 
+        // each testsRes element has double[](avg, best, worst) scores
+        ArrayList<double[]> testsRes = new ArrayList<double[]>();
 
-        return results;
+        for (int i = 0; i < Main.lessonModel.getChapters().size(); i++) {
+            // default test result vals
+            double [] testRes = new double[3];
+            Arrays.fill(testRes, 0.0);
+            String title = Main.lessonModel.getChapter(i).getTitle();
+            if (Main.quizzesModel.hasQuiz(title)) {
+                StudentModel.Student.Quiz quiz = Main.studentModel.getStudent(Main.username).getQuiz(i);
+
+                testRes[0] = quiz.getAverageScore();
+                testRes[1] = quiz.getBestScore();
+                testRes[2] = quiz.getWorstScore();
+            }
+
+            testsRes.add(testRes);
+        }
+
+        return testsRes;
     }
 }
