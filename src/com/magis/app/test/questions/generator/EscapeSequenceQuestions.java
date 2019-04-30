d440f2f45f784fa0a8be3716d56a115e5e607807
Naxhi <47636876+Naxhi@users.noreply.github.com>
2019-04-30 12:24:12 -0400

    Added files cause my branch is borked beyond belief

--- src/com/magis/app/test/questions/generator/EscapeSequenceQuestions.java
+++ src/com/magis/app/test/questions/generator/EscapeSequenceQuestions.java
@@ -25,12 +25,12 @@ public class EscapeSequenceQuestions extends QuestionGenerator{
         int sentencePicker = rand.nextInt(sentences.length);
         question = sentences[sentencePicker];
 
-        int sequenePicker = rand.nextInt(8);
+        int sequenePicker = rand.nextInt(5);
 
         switch(sequenePicker){
             case 0:
                 correctAnswer = question;
-                correctAnswer.replace('^', '\n');
+                correctAnswer = correctAnswer.replace('^', '\n');
                 answers.add(correctAnswer);
                 answers.add(question.replace('^','\t'));
                 answers.add(question.replace('^','\\'));
@@ -39,7 +39,7 @@ public class EscapeSequenceQuestions extends QuestionGenerator{
                 break;
             case 1:
                 correctAnswer = question;
-                correctAnswer.replace('^', '\t');
+                correctAnswer = correctAnswer.replace('^', '\t');
                 answers.add(correctAnswer);
                 answers.add(question.replace('^','\n'));
                 answers.add(question.replace('^','\\'));
@@ -48,7 +48,7 @@ public class EscapeSequenceQuestions extends QuestionGenerator{
                 break;
             case 2:
                 correctAnswer = question;
-                correctAnswer.replace('^', '\\');
+                correctAnswer = correctAnswer.replace('^', '\\');
                 answers.add(correctAnswer);
                 answers.add(question.replace('^','\t'));
                 answers.add(question.replace('^','\n'));
@@ -57,7 +57,7 @@ public class EscapeSequenceQuestions extends QuestionGenerator{
                 break;
             case 3:
                 correctAnswer = question;
-                correctAnswer.replace('^', '\"');
+                correctAnswer = correctAnswer.replace('^', '\"');
                 answers.add(correctAnswer);
                 answers.add(question.replace('^','\t'));
                 answers.add(question.replace('^','\n'));
@@ -66,21 +66,12 @@ public class EscapeSequenceQuestions extends QuestionGenerator{
                 break;
             case 4:
                 correctAnswer = question;
-                correctAnswer.replace('^', '\'');
+                correctAnswer = correctAnswer.replace('^', '\'');
                 answers.add(correctAnswer);
                 answers.add(question.replace('^','\t'));
                 answers.add(question.replace('^','\n'));
                 answers.add(question.replace('^','\"'));
                 question+="\n\nWhat will replacing the \"^\"(s) with a \" \\\' \" do to the string?";
-                break;
-            case 5:
-                correctAnswer = question;
-                correctAnswer.replace('^', '\\');
-                answers.add(correctAnswer);
-                answers.add(question.replace('^','\t'));
-                answers.add(question.replace('^','\n'));
-                answers.add("Turn the code into a single-line comment");
-                question+="\n\nWhat will replacing the \"^\"(s) with a \"\\\\\" do to the string?";
                 break;
         }
         Collections.shuffle(answers);
--- src/com/magis/app/test/questions/generator/QuestionGeneratorTest.java
+++ src/com/magis/app/test/questions/generator/QuestionGeneratorTest.java
@@ -186,5 +186,68 @@ class QuestionGeneratorTest {
         System.out.println("\nCorrect Answer: "+correctAnswer);
     }
 
+    @Test
+    void testArrays(){
+        ArraysQuestions objc  = new ArraysQuestions();
+        objc.getArrayIndexQuestion();
+        String[] answers = objc.getAnswers().toArray(new String[0]);
+        String question = objc.getQuestion();
+        String correctAnswer = objc.getCorrectAnswer();
+        System.out.println(question+"\n");
+
+        for(int i=0; i<answers.length; i++){
+            System.out.println((i+1)+". "+answers[i]);
+        }
+
+        System.out.println("\nCorrect Answer: "+correctAnswer);
+    }
+
+    @Test
+    void testArrays2(){
+        ArraysQuestions objc  = new ArraysQuestions();
+        objc.getIndexQuestion();
+        String[] answers = objc.getAnswers().toArray(new String[0]);
+        String question = objc.getQuestion();
+        String correctAnswer = objc.getCorrectAnswer();
+        System.out.println(question+"\n");
+
+        for(int i=0; i<answers.length; i++){
+            System.out.println((i+1)+". "+answers[i]);
+        }
+
+        System.out.println("\nCorrect Answer: "+correctAnswer);
+    }
+
+    @Test
+    void testEscapeSequences(){
+        EscapeSequenceQuestions objc  = new EscapeSequenceQuestions();
+        objc.getEscapeSequenceQuestion();
+        String[] answers = objc.getAnswers().toArray(new String[0]);
+        String question = objc.getQuestion();
+        String correctAnswer = objc.getCorrectAnswer();
+        System.out.println(question+"\n");
+
+        for(int i=0; i<answers.length; i++){
+            System.out.println((i+1)+". "+answers[i]);
+        }
+
+        System.out.println("\nCorrect Answer: "+correctAnswer);
+    }
+
+    @Test
+    void testConstructer(){
+        QuestionGenerator objc  = new ConstructerQuestions();
+        ((ConstructerQuestions) objc).generateContentQuestion();
+        String[] answers = objc.getAnswers().toArray(new String[0]);
+        String question = objc.getQuestion();
+        String correctAnswer = objc.getCorrectAnswer();
+        System.out.println(question+"\n");
+
+        for(int i=0; i<answers.length; i++){
+            System.out.println((i+1)+". "+answers[i]);
+        }
+
+        System.out.println("\nCorrect Answer: "+correctAnswer);
+    }
 
 }
