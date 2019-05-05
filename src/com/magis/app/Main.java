b99e9c2961837ccda4c99de18dc4fcac6994230b
Ian Spryn <4182500+ianspryn@users.noreply.github.com>
2019-05-05 01:05:03 -0400

    Replace diff library with a better one
    Seems promising

--- src/com/magis/app/Main.java
+++ src/com/magis/app/Main.java
@@ -1,11 +1,13 @@
 package com.magis.app;
 
+
 import com.magis.app.UI.UIComponents;
 import com.magis.app.login.Login;
 import com.magis.app.models.LessonModel;
 import com.magis.app.models.QuizzesModel;
 import com.magis.app.models.StudentModel;
 import com.magis.app.models.TestsModel;
+import com.magis.app.test.diff_match_patch;
 import com.magis.app.test.questions.generator.*;
 import javafx.application.Application;
 import javafx.scene.Parent;
@@ -13,8 +15,10 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+
 import java.awt.*;
 import java.util.HashMap;
+import java.util.LinkedList;
 
 public class Main extends Application {
 
@@ -106,5 +110,14 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) { launch(args); }
+
+//    public static void main(String[] args) {
+//        diff_match_patch dmp = new diff_match_patch();
+//        LinkedList<diff_match_patch.Diff> diff = dmp.diff_main("Hello World.", "Goodbye World.");
+//        // Result: [(-1, "Hell"), (1, "G"), (0, "o"), (1, "odbye"), (0, " World.")]
+//        dmp.diff_cleanupSemantic(diff);
+//        // Result: [(-1, "Hello"), (1, "Goodbye"), (0, " World.")]
+//        System.out.println(diff);
+//    }
 
 }
