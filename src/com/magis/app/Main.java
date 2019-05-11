2de36cf6f4dc003b74318e9efd1eb67b968982ac
GCC\WEINLANDJR1 <weinlandjr1@gcc.edu>
2019-05-11 14:22:15 -0400

    Fix imports

--- src/com/magis/app/Main.java
+++ src/com/magis/app/Main.java
@@ -6,7 +6,7 @@ import com.magis.app.login.Login;
 import com.magis.app.models.LessonModel;
 import com.magis.app.models.QuizzesModel;
 import com.magis.app.models.StudentModel;
-import com.magis.app.models.TestsModel;
+import com.magis.app.models.*;
 import com.magis.app.test.diff_match_patch;
 import com.magis.app.test.questions.generator.*;
 import javafx.application.Application;
@@ -110,14 +110,5 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) { launch(args); }
-
-//    public static void main(String[] args) {
-//        diff_match_patch dmp = new diff_match_patch();
-//        LinkedList<diff_match_patch.Diff> diff = dmp.diff_main("Hello World.", "Goodbye World.");
-//        // Result: [(-1, "Hell"), (1, "G"), (0, "o"), (1, "odbye"), (0, " World.")]
-//        dmp.diff_cleanupSemantic(diff);
-//        // Result: [(-1, "Hello"), (1, "Goodbye"), (0, " World.")]
-//        System.out.println(diff);
-//    }
 
 }
